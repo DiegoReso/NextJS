5809205864f0db2ec7c50da0fd4faaead2788109
Diego Reis <diegoreso@me.com>
2023-05-24 20:31:52 -0300

    create custom document and custom app

--- next/app/layout.js
+++ next/app/layout.js
@@ -10,8 +10,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={inter.className}>
+       
+      <h1>Titulo Fixo</h1>
+        {children}
+      
+      </body>
+      
     </html>
   )
 }
