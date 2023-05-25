05bfa442b4c1aefe6e565bdfbb900d12d716dda0
Diego Reis <diegoreso@me.com>
2023-05-24 22:36:44 -0300

    create listpost

--- next/app/listofposts/page.js
+++ next/app/listofposts/page.js
@@ -1,10 +1,19 @@
+const getPostsData = async () =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+  return res.json()
+}
 
 
-const ListOfPosts =()=>{
+
+const ListOfPosts = async ()=>{
+
+    const posts = await getPostsData()
+
+
   return(
-    <>
-
-    </>
+    <div>
+        {posts.map((post) => <p key={post.id}>{post.title}</p>)}
+    </div>
 
   )
 }
